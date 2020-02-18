70b17a09152d61c4a89765011548ecbf631b7791
Orphe650 <davortiz@gmail.com>
2020-02-18 10:40:43 +0100

    styles project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="layout">
+          <h1>ToDo List.</h1>
+          <div className="list">
+            <form>
+              <input type="text" placeholder="Add Item..." />
+              <button>
+                <i className="fas fa-plus"></i>
+              </button>
+            </form>
+            <ul>
+              <li>
+                Go surfing <i className="fas fa-minus-circle"></i>
+              </li>
+              <li className="done">
+                Study JavaScript <i className="fas fa-minus-circle"></i>
+              </li>
+              <li className="done">
+                Make coffee <i className="fas fa-minus-circle"></i>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
