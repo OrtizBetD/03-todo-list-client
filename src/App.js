c3c274322cf9377e7d5f5c09f206da7de43b9d09
Orphe650 <davortiz@gmail.com>
2020-02-20 06:02:35 +0100

    todo list deploy

--- src/App.js
+++ src/App.js
@@ -1,29 +1,117 @@
 import React from "react";
-import "./styles/styles.css";
+import "../src/styles/styles.css";
+import axios from "axios";
 
-class App extends React.Component {
+class Todo extends React.Component {
+  state = {
+    items: [],
+    name: ""
+  };
+
+  componentDidMount() {
+    axios
+      .get(`${process.env.REACT_APP_API}/items`)
+      .then(res => {
+        this.setState({
+          items: res.data.reverse()
+        });
+      })
+      .catch(err => {});
+  }
+  updateName = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+  addItem = e => {
+    e.preventDefault();
+    axios
+      .post(`${process.env.REACT_APP_API}/items`, { name: this.state.name })
+      .then(res => {
+        let items = this.state.items;
+        items.unshift(res.data);
+        this.setState({
+          items: items,
+          name: ""
+        });
+      });
+  };
+  toggleDone = x => {
+    let current = x.done;
+    let newValue = !current;
+
+    axios
+      .patch(`${process.env.REACT_APP_API}/items/${x._id}`, {
+        name: x.name,
+        done: newValue
+      })
+      .then(res => {
+        let item = res.data;
+        let items = this.state.items;
+        items.forEach((element, index) => {
+          if (element._id === item._id) {
+            items[index] = item;
+          }
+        });
+        this.setState({
+          items: items
+        });
+      });
+  };
+  getItemStyle = element => {
+    return element.done ? "done" : "";
+  };
+  deleteItem = x => {
+    axios
+      .delete(`${process.env.REACT_APP_API}/items/${x._id}`)
+      .then(res => {
+        let items = this.state.items;
+        let item = res.data;
+        items.forEach((element, index) => {
+          if (element._id === item._id) {
+            delete items[index];
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   render() {
     return (
       <>
         <div className="layout">
-          <h1>ToDo List.</h1>
+          <h1>
+            <strong>ToDo List.</strong>
+          </h1>
           <div className="list">
-            <form>
-              <input type="text" placeholder="Add Item..." />
+            <form onSubmit={this.addItem}>
+              <input
+                type="text"
+                placeholder="Add Item..."
+                value={this.state.name}
+                onChange={this.updateName}
+              />
               <button>
                 <i className="fas fa-plus"></i>
               </button>
             </form>
             <ul>
-              <li>
-                Go surfing <i className="fas fa-minus-circle"></i>
-              </li>
-              <li className="done">
-                Study JavaScript <i className="fas fa-minus-circle"></i>
-              </li>
-              <li className="done">
-                Make coffee <i className="fas fa-minus-circle"></i>
-              </li>
+              {this.state.items.map(element => {
+                return (
+                  <li
+                    className={this.getItemStyle(element)}
+                    key={element._id}
+                    onClick={x => this.toggleDone(element)}
+                  >
+                    {element.name}
+                    <i
+                      className="fas fa-minus-circle"
+                      onClick={x => this.deleteItem(element)}
+                    ></i>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
@@ -32,4 +120,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default Todo;
